77afcbc097dcab02d2f7e6f68dae741967d6f3c7
Richard Jones <richard.jones@cloudtechnologyoffice.com>
2024-01-25 12:03:41 +0000

    first commit

---
+++ data.tf
@@ -0,0 +1,62 @@
+data "aws_region" "current" {}
+
+data "aws_vpcs" "selected" {
+  filter {
+    # name   = "state"
+    # values = ["available"]
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+data "aws_subnets" "selected" {
+  filter {
+    name   = "vpc-id"
+    values = [local.vpc_id]
+  }
+  # filter {
+  #   name   = "tag:Name"
+  #   values = [var.subnet_name]
+  # }
+  #   filter {
+  #     name   = "cidr-block"
+  #     values = ["10.*"]
+  #   }
+}
+
+data "aws_security_groups" "selected" {
+  filter {
+    name = "vpc-id"
+    values = [
+      local.vpc_id
+    ]
+  }
+}
+
+data "aws_ami" "amz-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+
+locals {
+  vpc_id     = sort(data.aws_vpcs.selected.ids)[0]
+  subnet_ids = sort(data.aws_subnets.selected.ids)
+  security_group_ids = data.aws_security_groups.selected.ids
+}
